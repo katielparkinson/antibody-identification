986a60f0c7c379c3bd4cc89ca8e1290e892da990
Katie Parkinson <katieparkinson2406@gmail.com>
2026-04-18 19:06:09 -0600

    Fix prod compose and hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning>
         <header className="site-header">
           <Link className="brand" href="/">
             Antibody Identification
